a8df5f91483ebf294d045eeebda452c81d8c192e
pavol_potancok <pavol_potancok@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2006-07-24 11:24:16 +0000

    Outline view support added
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore-eclipse-ide/trunk@65 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- src/com/qoretechnologies/qore/editors/QoreEditor.java
+++ src/com/qoretechnologies/qore/editors/QoreEditor.java
@@ -4,61 +4,125 @@ import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.ResourceBundle;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.text.source.projection.ProjectionSupport;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.editors.text.TextEditor;
 import org.eclipse.ui.texteditor.ContentAssistAction;
 import org.eclipse.ui.texteditor.ITextEditorActionDefinitionIds;
 
-public class QoreEditor extends TextEditor {
+import com.qoretechnologies.qore.outline.EditorContentOutlinePage;
+
+public class QoreEditor extends TextEditor
+{
 
 	private ColorManager colorManager;
 
-	public QoreEditor() {
+	private EditorContentOutlinePage outlinePage;
+
+	private IEditorInput input;
+
+	public QoreEditor()
+	{
 		super();
 		colorManager = new ColorManager();
 		setSourceViewerConfiguration(new QoreConfiguration(colorManager));
 		setDocumentProvider(new QoreDocumentProvider());
 	}
 
-	public void dispose() {
+	public void dispose()
+	{
 		colorManager.dispose();
+		if (outlinePage != null)
+			outlinePage.setInput(null);
 		super.dispose();
 	}
+	
+	protected void editorSaved()
+	{
+		if (outlinePage != null)
+			outlinePage.update();	
+		super.editorSaved();
+	}
 
-	// #!PP^
-	protected void createActions() {
+	protected void doSetInput(IEditorInput newInput) throws CoreException
+	{
+		super.doSetInput(newInput);
+		this.input = newInput;
+
+		if (outlinePage != null)
+			outlinePage.setInput(input);
+	}
+
+	protected IDocument getInputDocument()
+	{
+		IDocument document = getDocumentProvider().getDocument(input);
+		return document;
+	}
+
+	public IEditorInput getInput()
+	{
+		return input;
+	}
+
+	/**
+	 * Supply our outline page impl if asked for, return the default adapter for other classes otherwise.
+	 */
+	public Object getAdapter(Class adapter)
+	{
+		if (adapter.getSimpleName().equals("IContentOutlinePage"))
+		{
+			if (outlinePage == null)
+			{
+				outlinePage = new EditorContentOutlinePage(this);
+				if (getEditorInput() != null)
+					outlinePage.setInput(getEditorInput());
+			}
+			return outlinePage;
+		}
+		return super.getAdapter(adapter);
+	}
+
+	protected void createActions()
+	{
 		super.createActions();
 		ResourceBundle rb = new ContentAssistResource();
-		Action action = new ContentAssistAction(rb, "ContentAssistProposal.",
-				this);
+		Action action = new ContentAssistAction(rb, "ContentAssistProposal.", this);
 		String id = ITextEditorActionDefinitionIds.CONTENT_ASSIST_PROPOSALS;
 		action.setActionDefinitionId(id);
 		setAction("ContentAssistProposal", action);
 		markAsStateDependentAction("ContentAssistProposal", true);
 	}
 
-	class ContentAssistResource extends ResourceBundle {
+	/**
+	 * Inner class for content assist initialization.
+	 * @author potancpa
+	 *
+	 */
+	class ContentAssistResource extends ResourceBundle
+	{
 
-		private Hashtable<String,String> labels = new Hashtable<String,String>();
+		private Hashtable<String, String> labels = new Hashtable<String, String>();
 
-		public ContentAssistResource() {
+		public ContentAssistResource()
+		{
 			super();
 			labels.put("ContentAssistProposal.label", "Content assist");
 			labels.put("ContentAssistProposal.tooltip", "Content assist");
-			labels.put("ContentAssistProposal.description",
-					"Provides Qore Content Assistance");
+			labels.put("ContentAssistProposal.description", "Provides Qore Content Assistance");
 		}
 
-		public Object handleGetObject(String key) {
+		public Object handleGetObject(String key)
+		{
 			return labels.get(key);
 		}
 
-		public Enumeration<String> getKeys() {
+		public Enumeration<String> getKeys()
+		{
 			return labels.keys();
 		}
 
 	}
-	// #!PP$
-
 }

4cec660d78e4a43190388ad747cc17228d396851
pavol_potancok <pavol_potancok@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2006-08-07 15:12:37 +0000

    minor refactoring
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore-eclipse-ide/trunk@111 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- src/com/qoretechnologies/qore/tools/ParserHelper.java
+++ src/com/qoretechnologies/qore/tools/ParserHelper.java
@@ -2,11 +2,27 @@ package com.qoretechnologies.qore.tools;
 
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.FindReplaceDocumentAdapter;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 
 public class ParserHelper
 {
-	public static int getBracketCount(FindReplaceDocumentAdapter finder, int currentOffset, int startOffset, String regexBracket) throws BadLocationException
+	/**
+	 * Searches from currentOffset backwards until startOffset, counting the
+	 * regexBracket occurences found.
+	 * 
+	 * @param finder
+	 * @param currentOffset
+	 *            offset at which the completion was invoked
+	 * @param startOffset
+	 *            offset in the document until which it should be searched
+	 *            (usually < than currentOffset)
+	 * @param regexBracket
+	 *            regext to search for
+	 * @return count of brackets found
+	 * @throws BadLocationException
+	 */
+	public static int getBracketCountBackwards(FindReplaceDocumentAdapter finder, int currentOffset, int startOffset, String regexBracket) throws BadLocationException
 	{
 		IRegion bracketRegion = null;
 		int bracketCount = 0;
@@ -23,6 +39,40 @@ public class ParserHelper
 		}
 		while (bracketRegion != null && lastOffsetForBrackets > startOffset);
 		return bracketCount;
+	}
+
+	/**
+	 * Return the offset of last closing bracket of the block starting from
+	 * fromOffset.
+	 * 
+	 * @param finder
+	 * @param fromOffset
+	 *            starts from this offset (e.g. "namespace ABC ...")
+	 * @return document offset of the last closing bracket
+	 * @throws BadLocationException
+	 */
+	public static int getNextBracketBlock(IDocument doc, FindReplaceDocumentAdapter finder, int fromOffset) throws BadLocationException
+	{
+		int endOffset = fromOffset;
+		IRegion bracketReg = null;
+        int opening = 0, closing = 0;
+        
+		do
+		{
+			bracketReg = finder.find(endOffset, "[{}]", true, false, false, true);
+			if (bracketReg != null)
+			{
+				switch (doc.getChar(bracketReg.getOffset()))
+				{
+					case '{':  opening++; break;
+					case '}':  closing++;
+					default : 
+				}
+				endOffset = bracketReg.getOffset()+1;
+			}
+		}
+		while (bracketReg != null && opening!=closing);
+		return endOffset;
 	}
 
 }

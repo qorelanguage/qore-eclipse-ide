d87738f2afbb011545ea2b87076a39996e4c5153
pavol_potancok <pavol_potancok@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2006-09-11 13:51:07 +0000

    getBracketCountBackwards() reimplemented without using FindReplaceDocumentAdapter.
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore-eclipse-ide/trunk@152 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- src/com/qoretechnologies/qore/tools/ParserHelper.java
+++ src/com/qoretechnologies/qore/tools/ParserHelper.java
@@ -10,41 +10,51 @@ public class ParserHelper
 	/**
 	 * Searches from currentOffset backwards until startOffset, counting the
 	 * regexBracket occurences found.
+	 * The method does not use FindReplaceDocumentAdapter class anymore due to performance issues.
 	 * 
-	 * @param finder
+	 * @param doc document to be searched through
 	 * @param currentOffset
 	 *            offset at which the completion was invoked
 	 * @param startOffset
 	 *            offset in the document until which it should be searched
 	 *            (usually < than currentOffset)
-	 * @param regexBracket
-	 *            regext to search for
+	 * @param bracket
+	 *            bracket to search for, i.e. '{'
 	 * @return count of brackets found
 	 * @throws BadLocationException
 	 */
-	public static int getBracketCountBackwards(FindReplaceDocumentAdapter finder, int currentOffset, int startOffset, String regexBracket) throws BadLocationException
+	public static int getBracketCountBackwards(IDocument doc, int currentOffset, int startOffset, char bracket) throws BadLocationException
 	{
-		IRegion bracketRegion = null;
 		int bracketCount = 0;
 		int lastOffsetForBrackets = currentOffset;
+
+		char found = (char) -1;
 		do
 		{
-			bracketRegion = finder.find(lastOffsetForBrackets, regexBracket, false, false, false, true);
-			if (bracketRegion != null)
+			try
 			{
-				lastOffsetForBrackets = bracketRegion.getOffset() - 1; // bracket.getLength('{')
-				if (lastOffsetForBrackets > startOffset)
-					bracketCount++;
+				found = doc.getChar(lastOffsetForBrackets);
+				lastOffsetForBrackets--;
+				if (found == bracket)
+				{
+					if (lastOffsetForBrackets > startOffset)
+						bracketCount++;
+				}
+			}
+			catch (Exception e)
+			{
+				// we reached beginning of the document, just return collected bracketCount
 			}
 		}
-		while (bracketRegion != null && lastOffsetForBrackets > startOffset);
+		while (lastOffsetForBrackets > startOffset);
 		return bracketCount;
 	}
 
 	/**
 	 * Return the offset of last closing bracket of the block starting from
-	 * fromOffset. A semicolon is also considered as end of a block, e.g. when defining a class like 
-	 * class OMQ::SomeClass; the methods follow not directly in a {} block, but are defined elsewhere. 
+	 * fromOffset. A semicolon is also considered as end of a block, e.g. when
+	 * defining a class like class OMQ::SomeClass; the methods follow not
+	 * directly in a {} block, but are defined elsewhere.
 	 * 
 	 * @param finder
 	 * @param fromOffset
@@ -56,41 +66,46 @@ public class ParserHelper
 	{
 		int endOffset = fromOffset;
 		IRegion bracketReg = null;
-        int opening = 0, closing = 0;
-        int nextSemicolOffset = -1;
-        boolean semicolFound = false;
-        
+		int opening = 0, closing = 0;
+		int nextSemicolOffset = -1;
+		boolean semicolFound = false;
+
 		bracketReg = finder.find(fromOffset, ";", true, false, false, false);
-		if(bracketReg!=null)
+		if (bracketReg != null)
 		{
 			nextSemicolOffset = bracketReg.getOffset();
 			semicolFound = true;
 		}
-		
+
 		do
 		{
 			bracketReg = finder.find(endOffset, "[{}]", true, false, false, true);
 			if (bracketReg != null)
 			{
-				// if the next semicolon comes before first {, consider it as and of block
-				if(semicolFound==true && bracketReg.getOffset() > nextSemicolOffset)
+				// if the next semicolon comes before first {, consider it as
+				// and of block
+				if (semicolFound == true && bracketReg.getOffset() > nextSemicolOffset)
 				{
 					endOffset = nextSemicolOffset;
 					break;
 				}
-				else // ignore the other semicolons inside {} block
-					semicolFound = false;  
-				
+				else
+					// ignore the other semicolons inside {} block
+					semicolFound = false;
+
 				switch (doc.getChar(bracketReg.getOffset()))
 				{
-					case '{':  opening++; break;
-					case '}':  closing++;
-					default : 
+				case '{':
+					opening++;
+					break;
+				case '}':
+					closing++;
+				default:
 				}
-				endOffset = bracketReg.getOffset()+1;
+				endOffset = bracketReg.getOffset() + 1;
 			}
 		}
-		while (bracketReg != null && opening!=closing);
+		while (bracketReg != null && opening != closing);
 		return endOffset;
 	}
 

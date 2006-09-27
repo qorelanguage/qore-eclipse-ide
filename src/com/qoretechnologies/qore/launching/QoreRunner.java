c866f5c37744d693cb39805939549d9631650220
pavol_potancok <pavol_potancok@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2006-09-27 10:59:33 +0000

    upd
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore-eclipse-ide/trunk@191 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- src/com/qoretechnologies/qore/launching/QoreRunner.java
+++ src/com/qoretechnologies/qore/launching/QoreRunner.java
@@ -19,27 +19,22 @@ import org.eclipse.ui.console.MessageConsoleStream;
 
 import com.qoretechnologies.qore.tools.EclipseTools;
 
-public class QoreRunner {
-	private final static Color CONSOLE_RED = new Color(Display.getDefault(),
-			new RGB(255, 0, 0));
+public class QoreRunner
+{
+	private final static Color CONSOLE_RED = new Color(Display.getDefault(), new RGB(255, 0, 0));
 
-	private final static Color CONSOLE_BLACK = new Color(Display.getDefault(),
-			new RGB(0, 0, 0));
+	private final static Color CONSOLE_BLACK = new Color(Display.getDefault(), new RGB(0, 0, 0));
 
-	private final static Color CONSOLE_BLUE = new Color(Display.getDefault(),
-			new RGB(0, 0, 255));
+	private final static Color CONSOLE_BLUE = new Color(Display.getDefault(), new RGB(0, 0, 255));
 
-	public static void executeQore(String scriptFile,
-			Map<String, Object> cmdLineOptions) {
+	public static void executeQore(String scriptFile, Map<String, Object> cmdLineOptions)
+	{
 		// check the scriptFile
 		if (scriptFile == null
-				&& (!cmdLineOptions.containsKey(K_chckDisplayVersion) || 
-						 (cmdLineOptions.containsKey(K_chckDisplayVersion) && ((Boolean)cmdLineOptions
-						.get(K_chckDisplayVersion)) == false))) {
-			MessageConsoleStream out = EclipseTools.getConsole()
-					.newMessageStream();
-			out
-					.println("There is nothing to run by qore. Please select or open a source file.");
+				&& (!cmdLineOptions.containsKey(K_chckDisplayVersion) || (cmdLineOptions.containsKey(K_chckDisplayVersion) && ((Boolean) cmdLineOptions.get(K_chckDisplayVersion)) == false)))
+		{
+			MessageConsoleStream out = EclipseTools.getConsole().newMessageStream();
+			out.println("There is nothing to run by qore. Please select or open a source file.");
 			return;
 		}
 		// prepare the command
@@ -47,14 +42,15 @@ public class QoreRunner {
 		qoreCmdLineElements.add(QORE_EXECUTABLE);
 
 		// use command line options if available from configuration
-		if (cmdLineOptions != null) {
-			for (String option : cmdLineOptions.keySet()) {
+		if (cmdLineOptions != null)
+		{
+			for (String option : cmdLineOptions.keySet())
+			{
 				if (cmdLineOptions.get(option) instanceof String)
-					qoreCmdLineElements.add((String) option
-							+ (String) cmdLineOptions.get(option));
-				else if (cmdLineOptions.get(option) instanceof Boolean
-						&& (Boolean) cmdLineOptions.get(option) == true)
-					qoreCmdLineElements.add(option);
+					qoreCmdLineElements.add((String) option + (String) cmdLineOptions.get(option));
+				else
+					if (cmdLineOptions.get(option) instanceof Boolean && (Boolean) cmdLineOptions.get(option) == true)
+						qoreCmdLineElements.add(option);
 			}
 		}
 		qoreCmdLineElements.add(scriptFile);
@@ -68,36 +64,38 @@ public class QoreRunner {
 		// inform that we are starting
 		MessageConsoleStream out = EclipseTools.getConsole().newMessageStream();
 		out.setColor(CONSOLE_BLACK);
-		out.println(SimpleDateFormat.getDateTimeInstance().format(new Date())
-				+ ": Executing: " + cmdLine + "");
+		out.println(SimpleDateFormat.getDateTimeInstance().format(new Date()) + ": Executing: " + cmdLine + "");
 
 		// construct & start the process
 		ProcessBuilder pb = new ProcessBuilder(qoreCmdLineElements);
 		pb.redirectErrorStream(true);
-		try {
+		try
+		{
 			long startTms = System.currentTimeMillis();
 			Process p = pb.start();
-			BufferedReader is = new BufferedReader(new InputStreamReader(p
-					.getInputStream()));
+			BufferedReader is = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			String s;
 			out = EclipseTools.getConsole().newMessageStream();
 			out.setColor(CONSOLE_BLUE);
 			while ((s = is.readLine()) != null)
 				out.println(s);
 			long endTms = System.currentTimeMillis();
-			try {
+			try
+			{
 				if (p.waitFor() != 0)
 					out.setColor(CONSOLE_RED);
-			} catch (InterruptedException e) {
+			}
+			catch (InterruptedException e)
+			{
 				// nothing, just let the color as it is
 			}
 			out = EclipseTools.getConsole().newMessageStream();
 			out.setColor(CONSOLE_BLACK);
-			out.println("Execution completed in "
-					+ new SimpleDateFormat("mm:ss SS ").format(new Date(endTms
-							- startTms)) + "msec.");
+			out.println("Execution completed in " + new SimpleDateFormat("mm:ss SS ").format(new Date(endTms - startTms)) + "msec.");
 			out.flush();
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			out = EclipseTools.getConsole().newMessageStream();
 			out.setColor(CONSOLE_RED);
 			out.println("Error during execution: " + e.getMessage());
